b103f63f042012c60805a95e060d8f906c20ebf0
Allan de Carvalho <allan.reges@fbiz.com.br>
2020-04-11 14:35:38 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import 'express-async-errors';
+import express from 'express';
+import path from 'path';
+import Youch from 'youch';
+import routes from './routes';
+
+import './database/index';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
---
+++ src/database/migrations/20200410013007-create-events.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('events', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      text: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('events');
+  },
+};
